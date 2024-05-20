31d0114e33ecb4795adcce3682e0b8eca9ba8ec1
Роман Кондрашов <roman.condrashov200015@gmail.com>
2024-05-20 22:28:58 +0300

    test commit

--- the_snake.py
+++ the_snake.py
@@ -3,10 +3,6 @@ from random import choice, randint
 import pygame
 
 # Константы для размеров поля и сетки:
-SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
-GRID_SIZE = 20
-GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
-GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
 # Направления движения:
 UP = (0, -1)
